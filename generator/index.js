427086bc962a04720d2fcdbd1ccad647d8ca31c8
Elad Cohen <eladc.android@gmail.com>
2019-02-19 15:44:51 +0200

    Add nativescript to package.json

--- generator/index.js
+++ generator/index.js
@@ -1,11 +1,20 @@
 module.exports = (api, options, rootOptions) => {
-  const fs = require('fs')
-  const rimraf = require('rimraf')
+  const fs = require('fs');
+  const rimraf = require('rimraf');
 
   api.extendPackage({
     scripts: {
       'watch:android': 'vue-cli-service tns --android',
-      'watch:ios': 'vue-cli-service tns --ios',
+      'watch:ios': 'vue-cli-service tns --ios'
+    },
+    nativescript: {
+      id: 'org.nativescript.application',
+      'tns-ios': {
+        version: '5.0.0'
+      },
+      'tns-android': {
+        version: '5.0.0'
+      }
     },
     dependencies: {
       'nativescript-vue': '^2.0.2'
@@ -15,31 +24,34 @@ module.exports = (api, options, rootOptions) => {
       'nativescript-vue-template-compiler': '^2.0.2',
       'tns-core-modules': '^5.2.0'
     }
-  })
+  });
 
   api.extendPackage(pkg => {
-    delete pkg.dependencies['vue']
-    delete pkg.devDependencies['vue-template-compiler']
-    delete pkg.browserslist
-    delete pkg.scripts['serve']
-  })
+    delete pkg.dependencies['vue'];
+    delete pkg.devDependencies['vue-template-compiler'];
+    delete pkg.browserslist;
+    delete pkg.scripts['serve'];
+  });
 
   api.render('./templates/simple', {
     doesCompile: api.generator.doesCompile,
     applicationName: api.generator.pkg.name,
     applicationVersion: api.generator.pkg.version,
-    applicationAndroidVersionCode: api.generator.pkg.version.split('.').join('0'),
-    applicationDescription: api.generator.pkg.description || api.generator.pkg.name,
+    applicationAndroidVersionCode: api.generator.pkg.version
+      .split('.')
+      .join('0'),
+    applicationDescription:
+      api.generator.pkg.description || api.generator.pkg.name,
     applicationLicense: api.generator.pkg.license || 'MIT',
     applicationId: options.applicationId,
-    historyMode: options.historyMode || false,
-  })
+    historyMode: options.historyMode || false
+  });
 
   // delete the "public" directory
   api.onCreateComplete(() => {
-    const publicPath = api.resolve('public')
-    if(fs.existsSync(publicPath)) {
-      rimraf.sync(publicPath)
+    const publicPath = api.resolve('public');
+    if (fs.existsSync(publicPath)) {
+      rimraf.sync(publicPath);
     }
-  })
-}
+  });
+};
